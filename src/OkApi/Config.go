cb907d044079a02b668248e020ebb9d6b9a0ee80
baoailian <baoailian@foxmail.com>
2016-06-24 15:08:11 +0800

    remove * and &

--- src/OkApi/Config.go
+++ src/OkApi/Config.go
@@ -2,27 +2,27 @@ package OkApi
 
 type Config struct {
 	//platform address
-	DisableSSL           *bool
-	GatewayHost          *string
-	HttpMethod           *string
+	DisableSSL  bool
+	GatewayHost string
+	HttpMethod  string
 
 	//System parameter name in HTTP header
-	AppKeyFieldName		 *string
-	AppVersionFieldName  *string
-	AppMarketIdFieldName *string
-	DeviceIdFieldName    *string
-	SessionIdFieldName   *string
-	SignFieldName        *string
+	AppKeyFieldName      string
+	AppVersionFieldName  string
+	AppMarketIdFieldName string
+	DeviceIdFieldName    string
+	SessionIdFieldName   string
+	SignFieldName        string
 
 	//System parameter name in HTTP body
-	ApiFieldName         *string
-	ParamsFieldName      *string
-	TimestampFieldName   *string
-	VersionFieldName     *string
+	ApiFieldName       string
+	ParamsFieldName    string
+	TimestampFieldName string
+	VersionFieldName   string
 
 	//System parameter value
-	AppVersionValue      *string
-	AppMarketIdValue     *string
+	AppVersionValue  string
+	AppMarketIdValue string
 }
 
 func NewConfig() *Config {
@@ -30,92 +30,92 @@ func NewConfig() *Config {
 		HttpMethod: "POST",
 
 		//System parameter name in HTTP header
-		AppKeyFieldName: "OA-App-Key",
-		AppVersionFieldName: "OA-App-Version",
+		AppKeyFieldName:      "OA-App-Key",
+		AppVersionFieldName:  "OA-App-Version",
 		AppMarketIdFieldName: "OA-App-Market-ID",
-		DeviceIdFieldName: "OA-Device-Id",
-		SessionIdFieldName: "OA-Session-Id",
-		SignFieldName: "OA-Sign",
+		DeviceIdFieldName:    "OA-Device-Id",
+		SessionIdFieldName:   "OA-Session-Id",
+		SignFieldName:        "OA-Sign",
 
 		//System parameter name in HTTP body
-		ApiFieldName: "api",
-		ParamsFieldName: "params",
+		ApiFieldName:       "api",
+		ParamsFieldName:    "params",
 		TimestampFieldName: "timestamp",
-		VersionFieldName: "version",
+		VersionFieldName:   "version",
 	}
 }
 
-func (c *Config) SetDisableSSL (disableSSL bool) *Config {
-	c.DisableSSL = &disableSSL
+func (c *Config) SetDisableSSL(disableSSL bool) *Config {
+	c.DisableSSL = disableSSL
 	return c
 }
 
 func (c *Config) SetGatewayHost(gatewayHost string) *Config {
-	c.GatewayHost = &gatewayHost
+	c.GatewayHost = gatewayHost
 	return c
 }
 
 func (c *Config) SetHttpMethod(httpMethod string) *Config {
-	c.HttpMethod = &httpMethod
+	c.HttpMethod = httpMethod
 	return c
 }
 
 func (c *Config) SetAppKeyFieldName(appkeyFieldName string) *Config {
-	c.AppVersionFieldName = &appkeyFieldName
+	c.AppVersionFieldName = appkeyFieldName
 	return c
 }
 
 func (c *Config) SetAppVersionFieldName(appVersionFieldName string) *Config {
-	c.AppVersionFieldName = &appVersionFieldName
+	c.AppVersionFieldName = appVersionFieldName
 	return c
 }
 
 func (c *Config) SetAppMarketIdFieldName(appMarketIdFieldName string) *Config {
-	c.AppMarketIdFieldName = &appMarketIdFieldName
+	c.AppMarketIdFieldName = appMarketIdFieldName
 	return c
 }
 
 func (c *Config) SetDeviceIdFieldName(deviceIdFieldName string) *Config {
-	c.DeviceIdFieldName = &deviceIdFieldName
+	c.DeviceIdFieldName = deviceIdFieldName
 	return c
 }
 
 func (c *Config) SetSessionIdFieldName(sessionIdFieldName string) *Config {
-	c.AppVersionFieldName = &sessionIdFieldName
+	c.AppVersionFieldName = sessionIdFieldName
 	return c
 }
 
 func (c *Config) SetSignFieldName(signFieldName string) *Config {
-	c.SignFieldName = &signFieldName
+	c.SignFieldName = signFieldName
 	return c
 }
 
 func (c *Config) SetApiFieldName(apiFieldName string) *Config {
-	c.ApiFieldName = &apiFieldName
+	c.ApiFieldName = apiFieldName
 	return c
 }
 
 func (c *Config) SetParamsFieldName(paramsFieldName string) *Config {
-	c.ParamsFieldName = &paramsFieldName
+	c.ParamsFieldName = paramsFieldName
 	return c
 }
 
 func (c *Config) SetTimestampFieldName(timestampFieldName string) *Config {
-	c.TimestampFieldName = &timestampFieldName
+	c.TimestampFieldName = timestampFieldName
 	return c
 }
 
 func (c *Config) SetVersionFieldName(versionFieldName string) *Config {
-	c.VersionFieldName = &versionFieldName
+	c.VersionFieldName = versionFieldName
 	return c
 }
 
 func (c *Config) SetAppVersionValue(appVersion string) *Config {
-	c.AppVersionValue = &appVersion
+	c.AppVersionValue = appVersion
 	return c
 }
 
 func (c *Config) SetAppMarketIdValue(appMarketId string) *Config {
-	c.AppMarketIdValue = &appMarketId
+	c.AppMarketIdValue = appMarketId
 	return c
 }
--- src/OkApi/Credential.go
+++ src/OkApi/Credential.go
@@ -1,14 +1,14 @@
 package OkApi
 
 type Credential struct {
-	AppKey *string
-	Secret *string
+	AppKey string
+	Secret string
 }
 
 func NewCredential(appKey string, secret string) *Credential {
 	return &Credential{
-		AppKey: &appKey,
-		Secret: &secret,
+		AppKey: appKey,
+		Secret: secret,
 	}
 }
 
@@ -16,7 +16,6 @@ func (c *Credential) SetAppKey(appKey string) *Credential {
 	c.AppKey = appKey
 	return c
 }
-
 
 func (c *Credential) SetSecret(secret string) *Credential {
 	c.AppKey = secret
