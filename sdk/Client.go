3e50c2c4c8351d1957eaa02c2f98fb810735595f
Qin Jianxiang <talkativedoggy@gmail.com>
2016-07-17 00:05:14 +0800

    remove unnessary variables declaration

--- sdk/Client.go
+++ sdk/Client.go
@@ -14,7 +14,7 @@ type Client struct {
 }
 
 func NewClient() *Client {
-	var client = &Client{
+	client := &Client{
 		HttpClient: http.DefaultClient,
 		RequestBuilder: &RequestBuilder{
 			Config:     &model.Config{},
@@ -31,45 +31,35 @@ func SetDefaultLogger(logger logger.LoggerInterface) {
 
 func (client *Client) CallApi(api string, version string, params interface{}, returnDataPointer interface{}) (*model.ApiResult, error) {
 	var apiResult *model.ApiResult
-	var err error
-	var request *http.Request
-	var response *http.Response
-	var responseBodyBytes []byte
-	var responseBodyString string
-
-	request, err = client.RequestBuilder.Build(api, version, params)
+	request, err := client.RequestBuilder.Build(api, version, params)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err = client.HttpClient.Do(request)
+	response, err := client.HttpClient.Do(request)
 	if err != nil {
 		DefaultLogger.Error("Failed to do http communication: " + err.Error())
 		return nil, err
 	}
 
 	defer response.Body.Close()
-	responseBodyBytes, err = ioutil.ReadAll(response.Body)
+	responseBodyBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		DefaultLogger.Error("Failed to read response body: " + err.Error())
 		return nil, err
 	}
-	responseBodyString = string(responseBodyBytes)
-	DefaultLogger.Debug("Response body: " + responseBodyString)
-
-	err = json.Unmarshal(responseBodyBytes, &apiResult)
-	if err != nil {
+	DefaultLogger.Debug("Response body: " + string(responseBodyBytes))
+	if err := json.Unmarshal(responseBodyBytes, &apiResult); err != nil {
 		DefaultLogger.Error("Reponse body is not valid json.")
 		return nil, err
 	}
 
+	//type casting
 	if returnDataPointer != nil {
-		responseBodyBytes, _ = json.Marshal(apiResult.Data)
-		err = json.Unmarshal(responseBodyBytes, returnDataPointer)
-		if err != nil {
+		responseDataBytes, _ := json.Marshal(apiResult.Data)
+		if err = json.Unmarshal(responseDataBytes, returnDataPointer); err != nil {
 			DefaultLogger.Error("Failed to cast return data type")
 		}
 	}
-
 	return apiResult, nil
 }
--- sdk/RequestBuilder.go
+++ sdk/RequestBuilder.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/OpsKitchen/ok_api_sdk_go/sdk/model"
-	"hash"
 	"io"
 	"net"
 	"net/http"
@@ -21,29 +20,24 @@ type RequestBuilder struct {
 	Credential *model.Credential
 }
 
-func (requestBuilder *RequestBuilder) Build(api string, version string, params interface{}) (*http.Request, error) {
-	var err error
-	var request *http.Request
-	var deviceId, gatewayUrl, paramJson, requestBody, timestamp string
-
-	paramJson, err = requestBuilder.getParamsJson(params)
+func (rb *RequestBuilder) Build(api string, version string, params interface{}) (*http.Request, error) {
+	paramJson, err := rb.getParamsJson(params)
+	if err != nil {
+		return nil, err
+	}
+	deviceId, err := rb.getDeviceId()
 	if err != nil {
 		return nil, err
 	}
 
-	deviceId, err = requestBuilder.getDeviceId()
-	if err != nil {
-		return nil, err
-	}
-
-	timestamp = requestBuilder.getTimestamp()
-	gatewayUrl = requestBuilder.getGatewayUrl()
-	requestBody = requestBuilder.getPostBody(api, version, paramJson, timestamp)
-	DefaultLogger.Debug("Gateway url:" + gatewayUrl)
-	DefaultLogger.Debug("Request body:" + requestBody)
+	timestamp := rb.getTimestamp()
+	gatewayUrl := rb.getGatewayUrl()
+	requestBody := rb.getPostBody(api, version, paramJson, timestamp)
+	DefaultLogger.Debug("Gateway url: " + gatewayUrl)
+	DefaultLogger.Debug("Request body: " + requestBody)
 
 	//init http request
-	request, err = http.NewRequest(requestBuilder.Config.HttpMethod, gatewayUrl, strings.NewReader(requestBody))
+	request, err := http.NewRequest(rb.Config.HttpMethod, gatewayUrl, strings.NewReader(requestBody));
 	if err != nil {
 		DefaultLogger.Error("Failed to create http request object: " + err.Error())
 		return nil, err
@@ -51,40 +45,36 @@ func (requestBuilder *RequestBuilder) Build(api string, version string, params i
 
 	//set headers
 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	request.Header.Set(requestBuilder.Config.AppKeyFieldName, requestBuilder.Credential.AppKey)
-	request.Header.Set(requestBuilder.Config.AppMarketIdFieldName, requestBuilder.Config.AppMarketIdValue)
-	request.Header.Set(requestBuilder.Config.AppVersionFieldName, requestBuilder.Config.AppVersionValue)
-	request.Header.Set(requestBuilder.Config.DeviceIdFieldName, deviceId)
-	request.Header.Set(requestBuilder.Config.SessionIdFieldName, requestBuilder.Credential.SessionId)
-	request.Header.Set(requestBuilder.Config.SignFieldName, requestBuilder.getSign(api, version, paramJson, timestamp))
+	request.Header.Set(rb.Config.AppKeyFieldName, rb.Credential.AppKey)
+	request.Header.Set(rb.Config.AppMarketIdFieldName, rb.Config.AppMarketIdValue)
+	request.Header.Set(rb.Config.AppVersionFieldName, rb.Config.AppVersionValue)
+	request.Header.Set(rb.Config.DeviceIdFieldName, deviceId)
+	request.Header.Set(rb.Config.SessionIdFieldName, rb.Credential.SessionId)
+	request.Header.Set(rb.Config.SignFieldName, rb.getSign(api, version, paramJson, timestamp))
 	DefaultLogger.Debug("Request header:", request.Header)
 	return request, nil
 }
 
-func (requestBuilder *RequestBuilder) getDeviceId() (string, error) {
-	var err error
-	var errMsg string
-	var interfaces []net.Interface
-	interfaces, err = net.Interfaces()
+func (rb *RequestBuilder) getDeviceId() (string, error) {
+	interfaces, err := net.Interfaces()
 	if err != nil {
 		DefaultLogger.Error("Failed to get net interface list: " + err.Error())
 		return "", err
 	}
 	if len(interfaces) < 2 {
-		errMsg = "Amount of net interfaces is less than 2"
+		errMsg := "Amount of net interfaces is less than 2"
 		DefaultLogger.Error(errMsg)
 		return "", errors.New(errMsg)
 	}
 	return interfaces[1].HardwareAddr.String(), nil
 }
 
-func (requestBuilder *RequestBuilder) getGatewayUrl() string {
-	var urlObj url.URL
-	urlObj = url.URL{
-		Host: requestBuilder.Config.GatewayHost,
-		Path: requestBuilder.Config.GatewayPath,
+func (rb *RequestBuilder) getGatewayUrl() string {
+	urlObj := url.URL{
+		Host: rb.Config.GatewayHost,
+		Path: rb.Config.GatewayPath,
 	}
-	if requestBuilder.Config.DisableSSL {
+	if rb.Config.DisableSSL {
 		urlObj.Scheme = "http"
 	} else {
 		urlObj.Scheme = "https"
@@ -92,34 +82,31 @@ func (requestBuilder *RequestBuilder) getGatewayUrl() string {
 	return urlObj.String()
 }
 
-func (requestBuilder *RequestBuilder) getParamsJson(params interface{}) (string, error) {
-	var err error
-	var jsonBytes []byte
-	jsonBytes, err = json.Marshal(params)
-	if err != nil {
+func (rb *RequestBuilder) getParamsJson(params interface{}) (string, error) {
+	jsonBytes, err := json.Marshal(params)
+	if  err != nil {
 		DefaultLogger.Error("Api parameter can not encode as json. Json encoder said: " + err.Error())
 		return "", err
 	}
 	return string(jsonBytes), nil
 }
 
-func (requestBuilder *RequestBuilder) getPostBody(api string, version string, paramJson string, timestamp string) string {
-	return fmt.Sprintf("%s&%s&%s&%s",
-		requestBuilder.Config.ApiFieldName+"="+api,
-		requestBuilder.Config.VersionFieldName+"="+version,
-		requestBuilder.Config.TimestampFieldName+"="+requestBuilder.getTimestamp(),
-		requestBuilder.Config.ParamsFieldName+"="+paramJson)
+func (rb *RequestBuilder) getPostBody(api string, version string, paramJson string, timestamp string) string {
+	str := fmt.Sprintf("%s&%s&%s", rb.Config.ApiFieldName+"="+api, rb.Config.VersionFieldName+"="+version,
+		rb.Config.TimestampFieldName+"="+rb.getTimestamp())
+	if paramJson != "null" {
+		str += "&" + rb.Config.ParamsFieldName+"="+paramJson
+	}
+	return str
 }
 
-func (requestBuilder *RequestBuilder) getSign(api string, version string, paramJson string, timestamp string) string {
-	var hashObj hash.Hash
-	var stringToBeSign string
-	hashObj = md5.New()
-	stringToBeSign = requestBuilder.Credential.Secret + api + version + paramJson + timestamp
+func (rb *RequestBuilder) getSign(api string, version string, paramJson string, timestamp string) string {
+	hashObj := md5.New()
+	stringToBeSign := rb.Credential.Secret + api + version + paramJson + timestamp
 	io.WriteString(hashObj, stringToBeSign)
 	return fmt.Sprintf("%x", hashObj.Sum(nil))
 }
 
-func (requestBuilder *RequestBuilder) getTimestamp() string {
+func (rb *RequestBuilder) getTimestamp() string {
 	return strconv.FormatInt(time.Now().Unix(), 10)
 }
