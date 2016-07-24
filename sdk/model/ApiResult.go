f859d74ed2b7ef9ad8c0a6fe52e4c2bbb7b0ca00
Qin Jianxiang <talkativedoggy@gmail.com>
2016-07-24 16:15:34 +0800

    omit empty json field when encoding

--- sdk/model/ApiResult.go
+++ sdk/model/ApiResult.go
@@ -2,7 +2,7 @@ package model
 
 type ApiResult struct {
 	Success      bool        `json:"success"`
-	ErrorCode    string      `json:"errorCode"`
-	ErrorMessage string      `json:"errorMessage"`
-	Data         interface{} `json:"data"`
+	ErrorCode    string      `json:"errorCode,omitempty"`
+	ErrorMessage string      `json:"errorMessage,omitempty"`
+	Data         interface{} `json:"data,omitempty"`
 }
